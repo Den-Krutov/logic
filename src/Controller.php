fc53f71531ed1395db2ae843049ecea853751ba1
Daniil Fedin <nniiyu@yandex.ru>
2025-03-19 17:43:03 +0300

    Create skeleton game

---
+++ src/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Den-Krutov\logic\Controller;
+
+use stxrsh1ne\logic\View;
+
+function startGame()
+{
+
+    View\displayStartScreen();
+}
---
+++ src/View.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Den-Krutov\logic\View;
+
+function displayStartScreen()
+{
+    echo "Welcome to the Logic Game!\n";
+}
